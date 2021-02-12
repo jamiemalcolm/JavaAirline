7dc75be801b90e4d93532a489780ce4d3401eeda
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-12 11:49:09 +0000

    enum planetype set up

--- src/main/java/enums/planeType.java
+++ src/main/java/enums/planeType.java
@@ -1,4 +1,24 @@
 package enums;
 
 public enum planeType {
+    BOEING747(400, 300),
+    BOEING737(200, 250),
+    BOEING727(100, 150),
+    AIRBUSA320(500, 450);
+
+    private final int capacity;
+    private final int weight;
+
+    planeType(int capacity, int weight) {
+        this.capacity = capacity;
+        this.weight = weight;
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
+
+    public int getWeight(){
+        return this.weight;
+    }
 }
