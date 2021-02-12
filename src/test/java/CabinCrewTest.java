6cd5b77af523e455f58bae40fa9fdfedb07154c5
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-12 11:18:29 +0000

    CabinCrew tests written

--- src/test/java/CabinCrewTest.java
+++ src/test/java/CabinCrewTest.java
@@ -1,2 +1,28 @@
+import enums.Rank;
+import org.junit.Before;
+import org.junit.Test;
+import people.CabinCrew;
+
+import static org.junit.Assert.assertEquals;
+
 public class CabinCrewTest {
+
+    CabinCrew cabinCrew;
+    CabinCrew cabinCrew2;
+    CabinCrew cabinCrew3;
+
+    @Before
+    public void before(){
+        cabinCrew = new CabinCrew("Frank", Rank.FLIGHTATTENDANT)
+        cabinCrew2 = new CabinCrew("Fran", Rank.FLIGHTATTENDANT)
+        cabinCrew3 = new CabinCrew("Fern", Rank.FLIGHTATTENDANT)
+    }
+    @Test
+    public void hasName(){
+        assertEquals("Frank", cabinCrew.getName());
+    }
+    @Test
+    public void hasRank(){
+        assertEquals(Rank.FLIGHTATTENDANT, cabinCrew2.getRank());
+    }
 }
