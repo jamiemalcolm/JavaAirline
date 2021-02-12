d0b748c8e104bccb3440c3ef57bf750c4126064c
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-12 16:55:29 +0000

    hashmap<Flight, int> added to passenger

--- src/main/java/people/Passenger.java
+++ src/main/java/people/Passenger.java
@@ -1,12 +1,18 @@
 package people;
 
+import flight.Flight;
+
+import java.util.HashMap;
+
 public class Passenger extends Person{
 
     private int numOfBags;
+    private HashMap<Flight, int> seatNumber;
 
     public Passenger(String name, int numOfBags){
         super(name);
         this.numOfBags = numOfBags;
+        this.seatNumber = new HashMap<Flight, int>();
     }
 
     public int getNumOfBags() {
